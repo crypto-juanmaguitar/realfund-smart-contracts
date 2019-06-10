eb9c3bb03fb548a6aa565bb040a68456db3369e0
JuanMa Garrido <juanma.garrido@gmail.com>
2019-06-10 12:18:15 +0300

    removed extra test

--- test/TokenSTP.test.js
+++ test/TokenSTP.test.js
@@ -38,18 +38,4 @@ contract('TokenProjectRealFund', accounts => {
     assert.equal(totalSupply.toNumber(), 100)
   })
 
-  it('should mint a given amount of tokens to a given address', async () => {
-    const result = await token.mint(creatorAccount, 100)
-
-    assert.equal(result.logs[0].event, 'Transfer')
-    assert.equal(result.logs[0].args.from.valueOf(), 0x0)
-    assert.equal(result.logs[0].args.to, creatorAccount)
-    assert.equal(result.logs[0].args.value.toNumber(), 100)
-
-    const balance0 = await token.balanceOf(creatorAccount)
-    assert.equal(balance0.toNumber(), 100)
-
-    const totalSupply = await token.totalSupply()
-    assert.equal(totalSupply.toNumber(), 100)
-  })
 })
